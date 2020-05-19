9464dfeebe70a0387f9647b741296359833a17f9
nathanlim45 <nathanlim45@gmail.com>
2020-05-19 13:26:51 +0000

    Project 3 - Part 1a / 5

--- main.cpp
+++ main.cpp
@@ -35,51 +35,51 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) horn
+        4) 'phone control' controls
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) honk
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) speedometer
+        2) fuel gauge
+        3) mileage
+        4) temperature gauge
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) check the current speed
+        2) check the current fuel level
+        3) check the total miles usage
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) air condition control
+        2) air circulation control
+        3) seat heating control
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) set indoor temperature
+        2) get outdoor air
+        3) heat the seat
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) music system
+        2) map system
+        3) phone system
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) listen to music
+        2) guide to destination
+        3) call
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) headrest
+        2) seat adjuster
+        3) seatbelt
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) recline the seat
+        2) put on seatbelt
 */
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
